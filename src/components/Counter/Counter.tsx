4dab0fd75769cde5ff67dc984e8062d1b6af7cd7
Jorge Daniel Sosa <jdsosa@gmail.com>
2021-08-09 18:33:47 +0100

    Added sold counter section

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,10 +1,24 @@
-import { Container } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
 import React from "react";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import "./Counter.scss";
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 
 export const Counter = () => {
   const { t } = useTranslation();
+
+  const image = useStaticQuery(graphql`
+    query Counter {
+      counter: file(relativePath: { eq: "sold.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
 
   return (
     <section id="counter" className="section-counter">
@@ -12,6 +26,11 @@ export const Counter = () => {
         <div className="counter">
           <span>{t("apartments.sold", { amount: 25 })}</span>
         </div>
+        <Grid container justifyContent="center">
+          <Grid item xs={12} sm={10} md={8}>
+            <Img fluid={image.counter.childImageSharp.fluid} />
+          </Grid>
+        </Grid>
       </Container>
     </section>
   );
